cfa2a0ae91c05e7a76f95de6c8ee0ebd8e460005
Kunika <157152440+kunikasirpor@users.noreply.github.com>
2025-05-20 19:42:28 +0530

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/authRoutes.js';
+import blogRoutes from './routes/blogRoutes.js';
+
+const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/auth', authRoutes);
+app.use('/blogs', blogRoutes);
+
+export default app;
